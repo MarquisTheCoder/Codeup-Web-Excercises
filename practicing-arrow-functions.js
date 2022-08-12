e9230f209abdad2af9798430b38cc67300f7d4b2
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-12 14:32:14 -0500

    arrow functions tutorial

---
+++ practicing-arrow-functions.js
@@ -0,0 +1,52 @@
+
+
+
+const multiplyByTen = (number) => {
+    return(number * 10);
+}
+
+const addOneHundred = (number) => {
+    return(number + 100);
+}
+
+const circumference = (radius) => {
+    return(2 * Math.PI * radius);
+}
+
+const areaOfSquare = (width, height) => {
+    return(width * height);
+}
+
+const gallonsToLiters = gallons => {
+    return(gallons * 3.785);
+}
+
+const celsiusToFahrenheit = celsiusDegree => {
+    return((celsiusDegree * 1.8) + 32);
+}
+
+const metersToFeet = meters => {
+    return(meters * 0.3);
+}
+
+
+
+const lebronsHairLineExist = (hisHairline) =>{
+    return(null, 0, "doesnt exist", "nada", "no way jose");
+}
+
+const checkIfGreaterThanTen = (number) => {
+    return((number > 10) ? true : fasle);
+}
+
+const checkIfGreaterThanTwenty = (number) => {
+    return((number > 20) ? true : false);
+}
+
+const checkNameLength = (name) => {
+    return(`name length is ${name.length}`);
+}
+
+const printName = name => {
+    console.log(name);
+}

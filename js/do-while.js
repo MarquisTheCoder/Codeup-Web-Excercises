bbb6f47bf7f0bcf621857590f2d9621bbf8e2b35
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-17 14:24:13 -0500

    autosave script

--- js/do-while.js
+++ js/do-while.js
@@ -25,11 +25,27 @@ Exercise for CodeUp LLC from Javier
 
 (function () {
 
-    const diceRoll = Math.floor(Math.random() * 6) + 1;
-    do {
-        alert('Wrong number');
-    }while(parseFloat(prompt(`Enter a number 1-6: `)) != diceRoll)
+    // const diceRoll = Math.floor(Math.random() * 6) + 1;
+    // do {
+    //     alert('Wrong number');
+    // }while(parseFloat(prompt(`Enter a number 1-6: `)) != diceRoll)
+    //
+    // alert(`You guessed it the number was ${diceRoll}`);
 
-    alert(`You guessed it the number was ${diceRoll}`);
+    // This is how you get a random number between 50 and 100
+    let allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+    console.log(`Amount of cones: ${allCones}`);
+    while(allCones > 0){
+        let bought  = Math.floor(Math.random() * 5) + 1;
+        if(bought > allCones){
+            console.log(`Cannot sell you ${bought}. I only have ${allCones}`);
+            allCones -= allCones;
+            console.log(`All cones sold!`);
+        }else{
+            allCones -= bought;
+            console.log(`${bought} cones sold! amount left: ${allCones}`);
+        }
 
+    }
 })();

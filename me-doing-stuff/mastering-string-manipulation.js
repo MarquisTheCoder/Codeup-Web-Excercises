4274aa052a2bd4702b267b16bff31c8afb0cbfee
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 12:43:28 -0500

    autosave script

---
+++ me-doing-stuff/mastering-string-manipulation.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+Creator: Deshawn Marquis, Williams,
+GitHub Profile: https://github.com/MarquisTheCoder,
+Date Created: 8/18/22,
+Time Created: 10:22 AM,
+File Name: mastering-string-manipulation.js,
+File Description: 
+*/
+
+
+(function () {
+
+    let childhoodFavorites= 'Naruto,Avatar The Last Air Bender, Drake & Josh, Zoey 101, Jack Black, Johnny Test';
+    let childHoodFavoroiteArray
+
+
+    const curseWords = ['shit','fuck','dick','bitch'];
+    while(!prompt('Tell us about you')){
+        alert('That\'s interesting to here!');
+    }
+    alert('THAT WORD IS NOT ALLOWED!!!');
+
+
+    let namesOfFamous = ['Angelina Jolie', 'Jordan Peterson', 'Jay Z', 'Scarlett Johanson', 'Andrew Tate'];
+    console.log(namesOfFamous.join(''))
+})();

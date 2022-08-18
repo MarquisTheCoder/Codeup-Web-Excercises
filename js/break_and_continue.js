0b24bc08821074dcc205cc03f9cd65727a99f059
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 13:38:55 -0500

    autosave script

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -11,11 +11,12 @@ Loop exercise for CodeUp LLC class.
 */
 (
     function () {
-    const checkRange = num => num >= 1 && num <= 50;
+    const check = num =>
+            (num >= 1 && num <= 50) && num % 2 !== 0;
 
     while(true){
         let number = prompt('Enter A number: ');
-        if(checkRange(number) && number % 2 !== 0){
+        if(check(number)){
             for(let odd = 1; odd < 50; odd += 2){
                 odd == number ?
                     console.log(`Yikes!Skipping number: ${number}`) :

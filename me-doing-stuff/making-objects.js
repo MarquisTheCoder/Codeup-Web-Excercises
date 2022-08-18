504d2e2878d613645dd5cd6deb25c87e61362cef
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 15:45:09 -0500

    autosave script

---
+++ me-doing-stuff/making-objects.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+ Creator: Deshawn Marquis, Williams,
+ GitHub Profile: https://github.com/MarquisTheCoder,
+ Date Created: 8/18/22,
+ Time Created: 2:32 PM,
+ File Name: making-objects.js,
+ File Description:
+ */
+
+
+(function () {
+        /*Using Javascript Object Literal Notation*/
+        let person = new Object;
+        person.postion = 'ceo';
+        person.employeeNumber = '5257234853453';
+        person
+})();

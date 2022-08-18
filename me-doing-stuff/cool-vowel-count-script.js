2908e0c15434f298ad52e23c2a9556dc971dddfb
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 14:16:47 -0500

    autosave script

--- me-doing-stuff/cool-vowel-count-script.js
+++ me-doing-stuff/cool-vowel-count-script.js
@@ -1,15 +1,16 @@
 "use strict";
- 
+
 /*
-Creator: Deshawn Marquis, Williams,
-GitHub Profile: https://github.com/MarquisTheCoder,
-Date Created: 8/18/22,
-Time Created: 2:10 PM,
-File Name: cool-vowel-count-script.js,
-File Description: 
-*/
+ Creator: Deshawn Marquis, Williams,
+ GitHub Profile: https://github.com/MarquisTheCoder,
+ Date Created: 8/18/22,
+ Time Created: 2:10 PM,
+ File Name: cool-vowel-count-script.js,
+ File Description:
+ */
 
 
-(function(){
-    
+(function () {
+        let vowelCount = str => (str.match(/[aeiou]/ig)||[]).length;
+        let vowelCount = str => str.match(/[aeiou]/ig)||[].length;
 })();

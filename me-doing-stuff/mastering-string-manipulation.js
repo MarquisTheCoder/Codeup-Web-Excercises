67e7d65947ed8dc3dab43d0d0c2be498d748e43b
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 12:48:48 -0500

    autosave script

--- me-doing-stuff/mastering-string-manipulation.js
+++ me-doing-stuff/mastering-string-manipulation.js
@@ -12,17 +12,18 @@ File Description:
 
 (function () {
 
-    let childhoodFavorites= 'Naruto,Avatar The Last Air Bender, Drake & Josh, Zoey 101, Jack Black, Johnny Test';
-    let childHoodFavoroiteArray
-
-
-    const curseWords = ['shit','fuck','dick','bitch'];
-    while(!prompt('Tell us about you')){
-        alert('That\'s interesting to here!');
-    }
-    alert('THAT WORD IS NOT ALLOWED!!!');
+    // let childhoodFavorites= 'Naruto,Avatar The Last Air Bender, Drake & Josh, Zoey 101, Jack Black, Johnny Test';
+    // let childHoodFavoroiteArray
+    //
+    //
+    // const curseWords = ['shit','fuck','dick','bitch'];
+    // while(!prompt('Tell us about you')){
+    //     alert('That\'s interesting to here!');
+    // }
+    // alert('THAT WORD IS NOT ALLOWED!!!');
 
 
     let namesOfFamous = ['Angelina Jolie', 'Jordan Peterson', 'Jay Z', 'Scarlett Johanson', 'Andrew Tate'];
-    console.log(namesOfFamous.join(''))
+    console.log(namesOfFamous.join(' '));
+    namesOfFamous
 })();

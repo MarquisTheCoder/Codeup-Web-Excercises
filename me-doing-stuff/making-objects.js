5e323f510f6671096790e482d026485d6c0a2677
Deshawn Williams <deshawn.williams516@icloud.com>
2022-08-18 22:50:15 -0500

    autosave script

--- me-doing-stuff/making-objects.js
+++ me-doing-stuff/making-objects.js
@@ -34,20 +34,25 @@
                            }
 
                 }
-                this.credentials = {
-                        userName: 'username',
-                        password: 'password'
-                }
+
                 this.privateData = {
-                        checkCredentials : function(){
-                                person.credentials.userName
+
+                        checkCredentials : function(userName, password){
+                                if(userName == 'user' && password == 'pass'){
+                                        return 'secret_key: 2342323k4ghv32';
+                                }
+                                return 'no access allowed'
                         }
                 }
                 this.userId = Math.floor(Math.random() * (10000000 - 1000 + 1) + 1000);
 
         }
+
         let Marquis = new person('Marquis', 'Williams', '02172000', 'Negro');
         Marquis.job.putOccupation('Developer');
         console.log(Marquis.job.occupation);
         console.log(Marquis.userId);
+
+        console.log(Marquis.privateData.checkCredentials('sdfs','sfdsf'));
+        console.log(Marquis.privateData.checkCredentials('user', 'pass'));
 })();

5713c9bf376a9e7b353b66d2f70d773008cb174c
Deshawn Williams <deshawn.williams516@icloud.com>
2022-09-26 09:41:28 -0500

    autosave script

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -9,16 +9,12 @@
  File Description:
  main mapbox api request and functionality
  */
+
+
 $(function() {
      
      
      let mapboxCount = 0;
-     
-     const MAPBOX_API_KEY= '';
-     
-     fetch('../data/config.json').then(function (config) {
-          MAPBOX_API_KEY = config.MAPBOX_API_KEY;
-     });
      
      function addRestaurantMap(address, name){
           
@@ -77,6 +73,8 @@ $(function() {
                     .setPopup(popup);
                })
      }
+     
+     //adding restaurants to the page
      addRestaurantMap('102 Navarro St, San Antonio, TX 78205', 'Thai Lucky');
      addRestaurantMap('2525 SW Loop 410, San Antonio, TX 78227', 'El Rodeo Mexican grill');
      addRestaurantMap('102 Navarro St, San Antonio, TX 78205', 'Thai Lucky');
@@ -86,7 +84,7 @@ $(function() {
      
      //generates latitude/longitude array for address
      function geocodeRestaurant(address, accessToken) {
-          var apiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+          let apiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
           return fetch(`${apiUrl}${encodeURIComponent(address)}.json?access_token=${accessToken}`)
           .then((response) => response.json())
           .then((responseData) => responseData.features[0].center);

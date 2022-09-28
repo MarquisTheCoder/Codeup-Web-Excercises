baff4c301f7b266c2c937cec4f328303e61d455d
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-09-28 12:02:54 -0500

    Complete revamp

--- js/weather_map.js
+++ js/weather_map.js
@@ -14,49 +14,40 @@ $(function(){
      
      const DEFAULT_CITY = 'San Antonio, TX'
      const MAP_CONTAINER = $('#map-container')
-     const WEATHER_INFO_CONTAINER = $('#weather-info-container')
+     const CARD_HOLDER = $('.card-holder')
      const SEARCH_BUTTON = $('#search-location-btn')
      
      updateScreen(DEFAULT_CITY, MAPBOX_API_KEY);
-     // $('#current-city').text(`${DEFAULT_CITY}`)
-     // let currentCity = DEFAULT_CITY
-     
-     
      
      //generating card to display map data
-     let generateCards = function(
-          date, temp, description, humidity, wind, pressure, icon){
-          let cardTemplate = `
-            <div class="card m-2 card-main" style="width: 18rem; background-color: #091A1F33">
-                <div class="card-body d-flex-column text-center">
-                     <h5 class="card-title text text-center" id="date"style="background-color: #12242933">${date}</h5>
-                     <h6 class="card-subtitle mb-2 text-white text-center mt-3">${parseInt(1.8*(parseInt(temp)-273) + 32)}&#8457;</h6>
-                     <img src="http://openweathermap.org/img/w/${icon}.png" alt="">
-                     <hr>
-                     <p class="card-text text-center data">Description - ${description}</p><hr>
-                     <p class="card-text text-center data">Humidity - ${humidity}</p><hr>
-                     <p class="card-text text-center data">Wind - ${wind}</p><hr>
-                     <p class="card-text text-center data">Pressure - ${pressure}</p>
-                </div>
-            </div>`
-          WEATHER_INFO_CONTAINER.append(cardTemplate)
-            
+     let generateCards = function( date, temp, description, humidity, wind, pressure, icon){
+          let cardTemplate = `<div class="card card-main" style=" width: 18rem; background-color: #15181D88; border: solid #010101cc 1px;">
+                                <div class="card-body d-flex-column text-center">
+                                     <h5 class="card-title text text-center" id="date"style="background-color: #00000033">${date}</h5>
+                                     <h6 class="card-subtitle mb-2 text-white text-center mt-3">${parseInt(1.8*(parseInt(temp)-273) + 32)}&#8457;</h6>
+                                     <img src="http://openweathermap.org/img/w/${icon}.png" alt="">
+                                     <hr>
+                                     <p class="card-text text-center data">Description - ${description}</p><hr>
+                                     <p class="card-text text-center data">Humidity - ${humidity}</p><hr>
+                                     <p class="card-text text-center data">Wind - ${wind}</p><hr>
+                                     <p class="card-text text-center data">Pressure - ${pressure}</p>
+                                </div>
+                             </div>`
+          
+          CARD_HOLDER.append(cardTemplate)
      }
      
-     //generating the latitude loingtitude form the 
-     // map box api 
+     //generating the latitude loingtitude from the mapbox_api
      function reverseGeocodeRestaurant(latitude, longitude, api_key){
                     let apiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
                     return fetch(`${apiUrl}${longitude},${latitude}.json?access_token=${api_key}`)
                          .then((response) => response.json())
                          .then((responseData) => responseData.features[0].place_name)
-               }
+     }
                
      function updateScreen(address, api_key) {
-           
-          geocodeRestaurant(address, api_key)
-          .then(mapCoordinates => {
-               
+          
+          geocodeRestaurant(address, api_key).then(mapCoordinates => {
                
                let longitude = mapCoordinates[0]
                let latitude = mapCoordinates[1]
@@ -70,18 +61,18 @@ $(function(){
                     `appid=${WEATHERMAP_API_KEY}`
               
                
-               //reinitializing the map per
-               //request
-              $('#map-container').html('')
+               //reinitializing the map per request
+              // $('#map-container').html('')
                mapboxgl.accessToken = MAPBOX_API_KEY;
                let map = new mapboxgl.Map({
                                                container: 'map-container',
-                                               style: 'mapbox://styles/mapbox/outdoors-v11',
-                                               center: [-98.4916, 29.4252]
+                                               style: 'mapbox://styles/mapbox/dark-v10',
+                                               center: [longitude, latitude],
+                                               zoom: 10  
+                                                  
                                           })
-               
+               CARD_HOLDER.html('') 
                $.get(apiCallUrl).done(response => {
-                    
                     //as the object array for the weather data in forecast is an
                     //array of 40 to get a 5 day forecast I had to filter by each 8th value
                     //and generate a card for each returned data set
@@ -102,10 +93,10 @@ $(function(){
                     })
                     
                     map.setCenter(mapCoordinates)
-                    map.setZoom(14)
-                    
+                    map.setZoom(10)
+                    map.addControl(new mapboxgl.NavigationControl());
                     //setting marker
-                    let marker = new mapboxgl.Marker({draggable: true})
+                    let marker = new mapboxgl.Marker({draggable: true, color: '#A74D1F'})
                          .setLngLat(mapCoordinates)
                          .addTo(map)
                     
@@ -115,11 +106,17 @@ $(function(){
                     //updates the screen with that place value
                     marker.on('dragend', () => {
                          reverseGeocodeRestaurant(marker.getLngLat().lat, marker.getLngLat().lng, api_key)
-                              .then(place =>{ 
-                                   $('#weather-info-container').html('')
+                              .then(place =>{
                                    updateScreen(place, api_key)
                               })
                     })
+                    map.on('click', (e) => {
+                         reverseGeocodeRestaurant(e.lngLat.lat, e.lngLat.lng,api_key)
+                         .then(place =>{
+                              updateScreen(place, api_key)
+                         })
+                    })
+     
                })
           
           
@@ -130,7 +127,8 @@ $(function(){
      //a human readable format
      function epochDateConversion(milliseconds){
           let date = new Date(milliseconds * 1000)
-          return (`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`)
+          // return (`${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`)
+          return `${date}`.split(' ')[0]
      } 
      
      //getting long/lat coordinaties for a specific address
@@ -144,9 +142,8 @@ $(function(){
     //on search button click updates information and map based
     // on input in #search-location input container 
      SEARCH_BUTTON.on('click',()=> {
-          WEATHER_INFO_CONTAINER.html('')
+          CARD_HOLDER.html('')
           updateScreen($('#search-location').val(), MAPBOX_API_KEY)
-          
           //deleting input text after search just a pet peeve
           $('#search-location').val('')
      })
